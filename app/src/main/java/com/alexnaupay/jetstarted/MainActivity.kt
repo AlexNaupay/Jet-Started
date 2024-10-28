a54d43ec1d7e62673de061d54e2fe3c197d1c08c
Alex Naupay <alexnaupay@gmail.com>
2024-10-27 19:20:35 -0500

    Row, Column

--- app/src/main/java/com/alexnaupay/jetstarted/MainActivity.kt
+++ app/src/main/java/com/alexnaupay/jetstarted/MainActivity.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import com.alexnaupay.jetstarted.ui.theme.JetStartedTheme
 
@@ -16,12 +21,31 @@ class MainActivity : ComponentActivity() {
         // enableEdgeToEdge()
         setContent {
             JetStartedTheme {
-                GenericText("Hello @alexh")
+                MyUI()
             }
         }
     }
 }
 
+@Composable
+fun MyUI() {
+    Row {
+        GenericImage()
+        Column {
+            GenericText("Hello @alexh")
+            GenericText("How Are You")
+        }
+    }
+}
+
+@Composable
+fun GenericImage() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_launcher_foreground),
+        contentDescription = "Foreground icon of Android"
+    )
+
+}
 
 @Composable
 fun GenericText(text: String, modifier: Modifier = Modifier) {
@@ -35,6 +59,6 @@ fun GenericText(text: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     JetStartedTheme {
-        GenericText("Hello @alexh")
+        MyUI()
     }
 }

4325d5012746aa9cd48689ae46738d46257608b0
Alex Naupay <alexnaupay@gmail.com>
2024-10-27 23:58:31 -0500

    Scroll with LazyColumn

--- app/src/main/java/com/alexnaupay/jetstarted/MainActivity.kt
+++ app/src/main/java/com/alexnaupay/jetstarted/MainActivity.kt
@@ -14,6 +14,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -22,27 +24,52 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.alexnaupay.jetstarted.ui.theme.BlueI
 import com.alexnaupay.jetstarted.ui.theme.JetStartedTheme
 
+private val messages: List<Message> = listOf(
+    Message("First message", "This is the first message"),
+    Message("Second message", "This is the second message"),
+    Message("Third message", "This is the third message"),
+    Message("Fourth message", "This is the fourth message"),
+    Message("Fifth message", "This is the fifth message"),
+    Message("Sixth message", "This is the sixth message"),
+    Message("Seventh message", "This is the seventh message"),
+    Message("Eighth message", "This is the eighth message"),
+    Message("Ninth message", "This is the ninth message"),
+    Message("Tenth message", "This is the tenth message"),
+    Message("Eleventh message", "This is the eleventh message"),
+    Message("Twelfth message", "This is the twelfth message"),
+    Message("Thirteenth message", "This is the thirteenth message"),
+)
+
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
             JetStartedTheme {
-                MyUI()
+                MessagesList(messages)
             }
         }
     }
 }
 
 @Composable
-fun MyUI() {
+fun MessagesList(messages: List<Message> = emptyList()){
+    LazyColumn {
+        items(messages) { message ->
+            MessageComponent(message)
+        }
+    }
+}
+
+@Composable
+fun MessageComponent(message: Message) {
     Row(
         modifier = Modifier
             .background(MaterialTheme.colorScheme.background)
@@ -52,12 +79,12 @@ fun MyUI() {
         Spacer(modifier = Modifier.width(8.dp))
         Column {
             GenericText(
-                "Hello @alexh",
+                text = message.title,
                 color = MaterialTheme.colorScheme.onBackground,
                 style = MaterialTheme.typography.titleMedium
             )
             Spacer(modifier = Modifier.height(4.dp))
-            GenericText("How Are You", color = BlueI)
+            GenericText(message.title, color = BlueI)
         }
     }
 }
@@ -90,11 +117,13 @@ fun GenericText(
     )
 }
 
-@Preview
-@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(showBackground = true, showSystemUi = true)
+//@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES,  showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     JetStartedTheme {
-        MyUI()
+        MessagesList(messages = messages)
     }
 }
+
+data class Message(val title: String, val body: String)
